22b92399239acd83fe2c3e94e73370be8e680e13
Recep Köseoğlu <recepkoseoglu@hotmail.com>
2017-03-07 13:20:21 +0200

    first commit

---
+++ src/LineChart.jsx
@@ -0,0 +1,240 @@
+import React from "react";
+import {ShallowComponent, Generator, Class, Arrays, Maps} from "robe-react-commons";
+import "./LineChart.css";
+import Legend from "./Legend";
+
+export default class AreaChart extends ShallowComponent {
+
+    static propTypes: Map = {
+        /**
+         * Width for chart as px
+         */
+        width: React.PropTypes.number,
+        /**
+         * Height for chart as px
+         */
+        height: React.PropTypes.number,
+        /**
+         * Data to be plotted on the chart
+         */
+        data: React.PropTypes.array,
+        /**
+         * Change to be made for the given data
+         */
+        meta: React.PropTypes.array
+    };
+
+    static defaultProps = {
+        width: 500,
+        height: 300,
+        data: [],
+        meta: []
+    };
+
+    legends = [];
+
+    constructor(props: Object) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div id="line" style={{marginLeft: 40}}>
+                <div className="rb-line-chart" style={{width: this.props.width, height: this.props.height}}>
+                    <svg className="rb-line-chart-svg">
+                        {this.__renderLines(this.props.data, this.props.meta)}
+                    </svg>
+                    <div className="tooltip" id="tooltip">Tooltip</div>
+                    <div className="rb-line-chart-axis">
+                        {this.__renderYAxis()}
+                    </div>
+                    <div className="rb-line-chart-axis">
+                        {this.__renderXAxis()}
+                    </div>
+                </div>
+                <Legend data={this.legends} width={this.props.width}/>
+            </div>
+        )
+    }
+
+    __renderLines(data: Array, meta: Array) {
+        let metaArr = [];
+        let xAxisWidth = this.__xAxisWidth();
+        let sumXAxisWidth = 0;
+
+        for (let i = 0; i < data.length - 1; i++) {
+            let item = data[i];
+            let itemArr = [];
+            let nexItem;
+
+            if (data[i + 1]) {
+                nexItem = data[i + 1];
+            }
+
+            let fields = this.__getFields(item);
+
+            for (let j in fields) {
+
+                let key = fields[j].key,
+                    value = fields[j].value,
+                    nexValue = value;
+
+                if (nexItem) {
+                    nexValue = nexItem[key];
+                }
+
+                let properties = Arrays.getValueByKey(meta, "dataKey", key);
+                properties = properties === undefined ? {} : properties;
+
+                let tooltip = item.name + "\n" + (properties.name || key) + " : " + value + " " + (properties.unit || "") + "\n",
+                    tooltipNext = nexItem.name + "\n" + (properties.name || key) + " : " + nexValue + " " + (properties.unit || "") + "\n";
+
+                let fill = properties.fill || this.__randColor(j)
+
+                this.legends[properties.name || key] = {fill: fill, label: properties.name || key};
+
+                let pointY = this.__pointY(value),
+                    nextPointY = this.__pointY(nexValue),
+                    x1 = sumXAxisWidth,
+                    y1 = pointY,
+                    x2 = sumXAxisWidth + xAxisWidth,
+                    y2 = nextPointY;
+
+                itemArr.push(
+                    <line
+                        key={key}
+                        x1={x1}
+                        y1={y1}
+                        x2={x2}
+                        y2={y2}
+                        data={tooltip + tooltipNext}
+                        onMouseOver={this.__showTooltip}
+                        onMouseOut={this.__hideTooltip}
+                        onMouseMove={this.__moveTooltip}
+                        strokeLinecap="round"
+                        stroke={fill}
+                        strokeWidth="4">
+                    </line>
+                );
+            }
+
+            sumXAxisWidth += xAxisWidth;
+
+            metaArr.push(
+                <g key={i}>
+                    {itemArr}
+                </g>)
+        }
+        return metaArr;
+    }
+
+
+    __renderYAxis() {
+        let maxYAxis = this.__maxYAxis();
+        let height = this.props.height / 4;
+        let axisArr = [];
+        for (let i = 0; i < 4; i++) {
+            axisArr.push(
+                <div key={i}
+                     id={parseInt((maxYAxis / 4) * (4 - i))}
+                     className="rb-line-y-axis"
+                     style={{height: height}}>
+                </div>);
+        }
+        return axisArr;
+    }
+
+    __renderXAxis() {
+        let data = this.props.data;
+        let width = this.__xAxisWidth();
+        let axisArr = [];
+        for (let i = 0; i < data.length - 1; i++) {
+            let item = data[i],
+                nextItem = data[i + 1] || {};
+            axisArr.push(
+                <div key={i}
+                     className="rb-line-x-axis"
+                     data={item.name}
+                     name={nextItem.name}
+                     style={{width: width}}>
+                </div>);
+        }
+        return axisArr;
+    }
+
+    __maxYAxis() {
+        let data = this.props.data;
+        let maxYAxis = 0;
+        for (let i in data) {
+            let fields = this.__getFields(data[i]);
+            for (let j in fields) {
+                if (fields[j].value > maxYAxis)
+                    maxYAxis = fields[j].value;
+            }
+        }
+        let a = maxYAxis > 1000 ? 1000 : maxYAxis > 100 ? 100 : maxYAxis > 50 ? 50 : maxYAxis > 10 ? 10 : 1;
+
+        return (~~((maxYAxis + a - 1) / a) * a);
+    }
+
+    __xAxisWidth() {
+        return (this.props.width - 1) / (this.props.data.length - 1);
+    }
+
+    __pointY(value: Number) {
+        let maxYAxis = this.__maxYAxis();
+        return ((this.props.height * ((value * 100) / maxYAxis)) / 100);
+    }
+
+    __getFields(data: Object) {
+        let arr = [];
+        for (let key in data) {
+            if (data.hasOwnProperty(key)) {
+                if (key === "name" || key === "fill" || key === "unit") {
+                    continue;
+                }
+                arr.push({
+                    value: data[key],
+                    key: key
+                });
+            }
+        }
+        return arr;
+    }
+
+    __showTooltip(e: Object) {
+        if (this.tooltip === undefined) {
+            this.tooltip = document.getElementById("tooltip");
+        }
+        this.tooltip.style.visibility = "visible";
+
+        let tooltipText = e.target.getAttribute("data");
+        let fill = e.target.getAttribute("stroke");
+
+        this.tooltip.innerHTML = tooltipText;
+        this.tooltip.style.backgroundColor = fill;
+    }
+
+    __hideTooltip(e: Object) {
+        if (this.tooltip === undefined)
+            this.tooltip = document.getElementById("tooltip");
+        this.tooltip.style.visibility = "hidden";
+    }
+
+    __moveTooltip(e: Object) {
+        if (this.tooltip === undefined)
+            this.tooltip = document.getElementById("tooltip");
+
+        this.tooltip.style.left = (e.clientX + 10) + "px";
+        this.tooltip.style.top = (e.clientY + 10) + "px";
+    }
+
+    __randColor(index: Number) {
+        let colors = ["#F44336", "#673AB7", "#2196F3", "#FF5722", "#9C27B0", "#FFC107", "#FF9800", "#4CAF50", "#00796B", "#009688", "#3F51B5"];
+        if (index !== undefined) {
+            return colors[index % colors.length];
+        }
+        return colors[Math.floor(Math.random() * (colors.length - 1))];
+    }
+
+}
---
+++ src/PieChart.jsx
@@ -0,0 +1,231 @@
+import React from "react";
+import {ShallowComponent, Generator, Class, Arrays, Maps} from "robe-react-commons";
+import "./PieChart.css";
+import Legend from "./Legend";
+import FaIcon from "../faicon/FaIcon";
+
+export default class PieChart extends ShallowComponent {
+
+    static propTypes: Map = {
+        /**
+         * Data to be plotted on the chart
+         */
+        data: React.PropTypes.array,
+        /**
+         * Size of chart as px
+         */
+        size: React.PropTypes.number
+    };
+
+    static defaultProps = {
+        size: 400,
+        data: []
+    };
+
+    legends = [];
+
+    constructor(props: Object) {
+        super(props);
+        this.state = {
+            data: this.props.data,
+            clicked: false
+        }
+    }
+
+    componentWillReceiveProps(nextProps: Object) {
+        this.setState({data: nextProps.data})
+    }
+
+    render() {
+        let data = this.state.data;
+        let depth = this.__depthTree(data);
+        this.legends = [];
+
+        let c = 2 * depth + 1;
+        let mRadius = (((this.props.size - 1) / 2) / c) * ( c - depth - 1);
+
+        let root = depth > 1 ?
+            (<circle
+                fill="white"
+                cx={this.props.size / 2}
+                cy={this.props.size / 2}
+                r={mRadius}/>) :
+            undefined;
+
+        return (
+            <div className="rb-pie-chart" style={{width: this.props.size, height: this.props.size}}>
+                <div className="rb-pie-chart-back-tool" style={{display: this.state.clicked ? "inherit" : "none"}}>
+                    <FaIcon code="fa-undo" onClick={this.__onClickReset}/>
+                </div>
+                <svg className="rb-pie-chart-svg">
+                    {this.__renderPies(data, 360, 0, depth, depth - 1)}
+                    {root}
+                </svg>
+                <div className="tooltip" id="tooltip"></div>
+                <Legend data={this.legends} width={this.props.size}/>
+            </div>
+        )
+    }
+
+    __renderPies(data: Array, percentage: Number, rotation: Number, depth: Number, depthIndex: Number) {
+        if (!data || data.length <= 0)
+            return [];
+
+        let piesArr = [],
+            sumValues = this.__sumValues(data),
+            mRotation = rotation;
+
+        let c = 2 * depth + 1;
+        let mRadius = (((this.props.size - 1) / 2) / c) * ( c - depthIndex);
+
+        data.map(function (item, key) {
+            let mPercentage = (percentage * item.value) / sumValues;
+            piesArr.push.apply(piesArr, this.__renderPies(item.children, mPercentage, mRotation, depth, depthIndex - 1));
+            mRotation += mPercentage;
+        }.bind(this));
+
+        piesArr.push.apply(piesArr, this.__createPath(data, mRadius, percentage, rotation, depth, depthIndex));
+        return piesArr;
+    }
+
+    __createPath(data: Array, radius: Number, percentage: Number, rotaion: Number, depth: Number, depthIndex: Number) {
+        let sectors = [],
+            mRadius = radius,
+            origin = this.props.size / 2,
+            mRotation = rotaion,
+            max = this.__sumValues(data);
+
+        data.map(function (item, key) {
+            let value = item.value,
+                mPercentage = (percentage * value) / max,
+                isCircle = max === value,
+                aCalc = ( mPercentage > 180 ) ? 360 - mPercentage : mPercentage,
+                aRad = aCalc * Math.PI / 180,
+                z = Math.sqrt(2 * mRadius * mRadius - ( 2 * mRadius * mRadius * Math.cos(aRad) )),
+                x = aCalc <= 90 ? mRadius * Math.sin(aRad) : mRadius * Math.sin((180 - aCalc) * Math.PI / 180),
+                y = Math.sqrt(z * z - x * x),
+                Y = (origin - mRadius) + y,
+                X = mPercentage <= 180 ? origin + x : origin - x,
+                arcSweep = mPercentage <= 180 ? 0 : 1,
+                V = origin - mRadius,
+                fill = item.fill || this.__randColor(key + z),
+                tooltip = item.label + "  " + value + " " + (item.unit || "") + "\n" + " " + (((mPercentage * 100) / 360).toFixed(2) + " %"),
+                cursor = "default";
+
+            item.fill = fill;
+            if (depthIndex === (depth - 1)) {
+                this.legends[item.key || item.label] = {fill: fill, label: item.label};
+                cursor = "pointer";
+            }
+            if (isCircle)
+                sectors.push(
+                    <circle
+                        key={item.key + key + mPercentage}
+                        id={item.key}
+                        fill={fill}
+                        cx={origin}
+                        cy={origin}
+                        r={radius}
+                        data={tooltip}
+                        onMouseOver={this.__showTooltip}
+                        onMouseOut={this.__hideTooltip}
+                        onMouseMove={this.__moveTooltip}
+                        onClick={this.__onClick.bind(undefined, item)}/>);
+
+            else {
+                sectors.push(
+                    <path
+                        key={item.key + key + mPercentage}
+                        id={item.key}
+                        transform={'rotate(' + mRotation + ', ' + origin + ', ' + origin + ')'}
+                        d={'M ' + origin + ' ' + origin + ' V ' + V + ' A ' + mRadius + ' ' + mRadius + ' 1 ' + arcSweep + ' 1 ' + X + '  ' + Y + " z"}
+                        fill={fill}
+                        data={tooltip}
+                        onMouseOver={this.__showTooltip}
+                        onMouseOut={this.__hideTooltip}
+                        onMouseMove={this.__moveTooltip}
+                        style={{cursor: cursor}}
+                        onClick={this.__onClick.bind(undefined, item)}>
+                        <animateTransform
+                            attributeName="transform"
+                            type="rotate"
+                            from={ 0 + ', ' + origin + ', ' + origin }
+                            to={ mRotation + ', ' + origin + ', ' + origin }
+                            dur="1s"
+                            fill="freeze"
+                        />
+                    </path>);
+            }
+            mRotation = mRotation + mPercentage;
+        }.bind(this));
+        return sectors
+    }
+
+    __onClick(data: Object) {
+        let arr = [];
+        arr.push(data);
+        let depth = this.__depthTree(arr);
+        if (depth > 1) {
+            this.setState({data: arr, clicked: true});
+        }
+    }
+
+    __onClickReset() {
+        this.setState({data: this.props.data, clicked: false})
+    }
+
+    __depthTree(data: Array) {
+        if (!data || data.length <= 0) {
+            return 0;
+        }
+        let depth = 0;
+        for (let i = 0; i < data.length; i++) {
+            depth = Math.max(depth, this.__depthTree(data[i].children));
+        }
+        return 1 + depth;
+    }
+
+    __sumValues(data: Array) {
+        let max = 0;
+        data.map(function (item, key) {
+            let value = item.value;
+            max += value;
+        });
+        return max;
+    }
+
+    __showTooltip(e: Object) {
+        if (this.tooltip === undefined) {
+            this.tooltip = document.getElementById("tooltip");
+        }
+        this.tooltip.style.visibility = "visible";
+
+        let tooltipText = e.target.getAttribute("data");
+        let fill = e.target.getAttribute("fill");
+
+        this.tooltip.innerHTML = tooltipText;
+        this.tooltip.style.backgroundColor = fill;
+    }
+
+    __hideTooltip(e: Object) {
+        if (this.tooltip === undefined)
+            this.tooltip = document.getElementById("tooltip");
+        this.tooltip.style.visibility = "hidden";
+    }
+
+    __moveTooltip(e: Object) {
+        if (this.tooltip === undefined)
+            this.tooltip = document.getElementById("tooltip");
+
+        this.tooltip.style.left = (e.clientX + 10) + "px";
+        this.tooltip.style.top = (e.clientY + 10) + "px";
+    }
+
+    __randColor(index: Number) {
+        let colors = ["#F44336", "#2196F3", "#E91E63", "#00BCD4", "#673AB7", "#009688", "#3F51B5", "#4CAF50", "#FF9800", "#FF5722", "#FFC107"];
+        if (index !== undefined) {
+            return colors[parseInt(Math.abs(index % colors.length)) % (colors.length)];
+        }
+        return colors[Math.floor(Math.random() * (colors.length - 1))];
+    }
+}
